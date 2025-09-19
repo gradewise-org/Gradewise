33998b17a4cb6c70cbf838ec407f47d0765c3619
Joshua Delgado <100707532+Joshuad02@users.noreply.github.com>
2025-09-19 03:44:19 -0400

    Initial open API setup, adjusted temporal client setup to be passed in, and changed makefile to get rid of time (#16)

--- backend/internal/api/impl.go
+++ backend/internal/api/impl.go
@@ -1,78 +1,65 @@
 package api
 
 import (
-	"context"
 	"gradewise/backend/internal/temporal"
 	"gradewise/backend/internal/temporal/workflow"
-	"log"
 	"net/http"
 
 	"github.com/gin-gonic/gin"
+	"github.com/google/uuid"
 	"go.temporal.io/sdk/client"
 )
 
 // Ensure that we've conformed to the `ServerInterface` with a compile-time check
 var _ ServerInterface = (*Server)(nil)
 
-type Server struct{}
-
-func NewServer() Server {
-	return Server{}
+type Server struct {
+	Temporal client.Client
 }
 
-func (Server) GreetUser(c *gin.Context, params GreetUserParams) {
-	// Create Temporal client
-	t, err := client.Dial(client.Options{
-		HostPort: temporal.GetTemporalAddress(),
-	})
-	if err != nil {
-		log.Println("Unable to create client", err)
-		c.Status(http.StatusInternalServerError)
+func NewServer(t client.Client) *Server {
+	return &Server{Temporal: t}
+}
+
+func (s *Server) RegisterFaculty(c *gin.Context) {
+	var req FacultyRegistrationRequest
+	if err := c.ShouldBindJSON(&req); err != nil {
+		c.JSON(http.StatusBadRequest, Error{Code: "BAD_REQUEST", Message: "invalid JSON body"})
 		return
 	}
-	defer t.Close()
 
-	we, err := t.ExecuteWorkflow(
-		context.Background(),
-		client.StartWorkflowOptions{
-			// ID: "",
-			TaskQueue: temporal.TaskQueueName,
-		},
+	// TODO: hash password, insert into Postgres, create verification code, send email... basically do everyting useful lol
+	c.JSON(http.StatusCreated, FacultyRegistrationResponse{
+		FacultyId: uuid.MustParse(uuid.NewString()), // matches openapi_types.UUID
+		Email:     req.Email,
+		Status:    PENDINGVERIFICATION,
+	})
+}
+
+func (s *Server) GreetUser(c *gin.Context, params GreetUserParams) {
+	if s.Temporal == nil {
+		c.String(http.StatusServiceUnavailable, "Temporal unavailable")
+		return
+	}
+
+	ctx := c.Request.Context()
+
+	we, err := s.Temporal.ExecuteWorkflow(
+		ctx,
+		client.StartWorkflowOptions{TaskQueue: temporal.TaskQueueName},
 		workflow.InteractWithMe,
 		params.Name,
 	)
 	if err != nil {
-		log.Println("Unable to execute workflow", err)
 		c.Status(http.StatusInternalServerError)
 		return
 	}
 
-	var workflowResult string
-	// err = we.Get(context.Background(), &workflowResult)
-	desc, err := t.DescribeWorkflowExecution(context.Background(), we.GetID(), we.GetRunID())
-	if err != nil {
-		log.Println("Unable to describe workflow execution")
+	var result string
+	if err := s.Temporal.GetWorkflow(ctx, we.GetID(), we.GetRunID()).Get(ctx, &result); err != nil {
 		c.Status(http.StatusInternalServerError)
 		return
 	}
 
-	log.Printf("Status = %d", desc.WorkflowExecutionInfo.Status)
-
-	err = t.GetWorkflow(context.Background(), we.GetID(), we.GetRunID()).Get(context.Background(), &workflowResult)
-	if err != nil {
-		log.Println("Unable to get workflow result", err)
-		c.Status(http.StatusInternalServerError)
-		return
-	}
-
-	desc, err = t.DescribeWorkflowExecution(context.Background(), we.GetID(), we.GetRunID())
-	if err != nil {
-		log.Println("Unable to describe workflow execution")
-		c.Status(http.StatusInternalServerError)
-		return
-	}
-
-	log.Printf("Status = %d", desc.WorkflowExecutionInfo.Status)
-
-	c.JSON(http.StatusOK, workflowResult)
+	c.JSON(http.StatusOK, result)
 }
