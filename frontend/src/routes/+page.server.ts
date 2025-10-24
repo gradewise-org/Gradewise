dede28b53cebdbaa3c2abcbe5afb54698e1586d0
jonathan-gut <jonathangutierrezmcc@gmail.com>
2025-10-23 23:31:34 -0400

    Refrence service by DNS

--- frontend/src/routes/+page.server.ts
+++ frontend/src/routes/+page.server.ts
@@ -2,7 +2,7 @@ import type { PageServerLoad } from './$types';
 import { env } from '$env/dynamic/private';
 
 export const load: PageServerLoad = async () => {
-  const base = env.API_URL ?? 'http://127.0.0.1:18081';
+  const base = env.API_URL ?? 'http://gradewise-api-backend';
   try {
     // TODO: replace '/health' with your real endpoint
     const r = await fetch(`${base}/health`);
