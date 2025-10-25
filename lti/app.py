b6114b98254d847c043dcbcd357e90fe8ae624c6
jonathan-gut <jonathangutierrezmcc@gmail.com>
2025-10-24 21:11:00 -0400

    Switch port on LTI app to 8085 to match cloudflared route

--- lti/app.py
+++ lti/app.py
@@ -153,4 +153,4 @@ def add_csp(resp):
     return resp
 
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=int(os.environ.get("PORT", 8080)))
+    app.run(host="0.0.0.0", port=int(os.environ.get("PORT", 8085)))
